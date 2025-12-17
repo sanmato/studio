6cff886dcc666ee1c62d7d09b4188a8213a7ce45
Santiago Mato <santiago.mato.rpc@gmail.com>
2025-12-17 17:57:53 +0000

    ponle una opacity de 40, además estila unos bordes circulares leves

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -3,13 +3,13 @@ import Image from 'next/image';
 
 export function Logo(props: SVGProps<SVGSVGElement>) {
   return (
-    <div className="relative flex items-center justify-center w-[80px] h-[80px]" {...props}>
+    <div className="relative flex items-center justify-center w-[80px] h-[80px] rounded-lg overflow-hidden" {...props}>
       <Image
         src="/napkin-bg.png"
         alt="Napkin drawing background"
         width={80}
         height={80}
-        className="absolute inset-0 object-contain opacity-50"
+        className="absolute inset-0 object-contain opacity-40"
       />
       <span className="relative font-logo font-bold text-2xl tracking-tight" style={{ color: '#1C3F5E' }}>.merlo</span>
     </div>
