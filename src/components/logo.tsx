ab0c1dd67fd78822521b70ab386c3b3bb99f01e9
Santiago Mato <santiago.mato.rpc@gmail.com>
2025-12-17 17:52:49 +0000

    debes hacer lo siguiente:
    1- Reducir el tamaño, porque se ve sobresalie

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -3,15 +3,15 @@ import Image from 'next/image';
 
 export function Logo(props: SVGProps<SVGSVGElement>) {
   return (
-    <div className="relative flex items-center justify-center w-[160px] h-[160px]" {...props}>
+    <div className="relative flex items-center justify-center w-[80px] h-[80px]" {...props}>
       <Image
         src="/napkin-bg.png"
         alt="Napkin drawing background"
-        width={160}
-        height={160}
-        className="absolute inset-0 object-contain"
+        width={80}
+        height={80}
+        className="absolute inset-0 object-contain opacity-70"
       />
-      <span className="relative font-logo font-bold text-5xl tracking-tight" style={{ color: '#1C3F5E' }}>.merlo</span>
+      <span className="relative font-logo font-bold text-2xl tracking-tight" style={{ color: '#1C3F5E' }}>.merlo</span>
     </div>
   );
 }
