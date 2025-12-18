21adeec46d13a8574b3d7e160c31a37e0c72f7c5
Santiago Mato <santiago.mato.rpc@gmail.com>
2025-12-18 17:18:50 +0000

    me gustaría aplicar el favicon, usar el mismo logo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,6 +18,7 @@ export default function RootLayout({
   return (
     <html lang="es" className="scroll-smooth">
       <head>
+        <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700;900&family=Poppins:wght@400;700&display=swap" rel="stylesheet" />
